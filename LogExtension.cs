41172cf4d82e99c3f8f334c93031e1461def6ccb
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2024-09-27 15:22:36 -0300

    Add project files.

---
+++ LogExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LogGetMethodName;
+
+public static class LogExtension
+{
+    public static void Logue([CallerMemberName] string nomeMetodo = null)
+    {
+        Console.WriteLine($"\n Método - {nomeMetodo}: {DateTime.Now}");
+    }
+}
